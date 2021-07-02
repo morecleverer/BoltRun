ea127db6f4a6e1ddf9a94808c1a651a187eaeabc
morecleverer <kimdk0824@naver.com>
2021-07-02 21:42:58 +0900

    finish

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float level(int score)
+    {
+        int defaultSpeed = -6;
+        int increSpeed = (score * -1) / 100;
+        if (increSpeed < -6)
+            increSpeed = -6;
+
+        return defaultSpeed + increSpeed;
+    }
+    
+}
